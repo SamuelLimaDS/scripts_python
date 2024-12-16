ff9ef276e6df09b07195ab2153cb544ce9787bca
Samuel Lima dos Santos <samuel.santos@lnls.br>
2024-12-16 15:11:34 -0300

    teste commit

---
+++ hello_python.py
@@ -0,0 +1,16 @@
+# Code date time
+
+from datetime import  datetime #import bilbioteca date time
+
+
+#function date timea
+
+def my_function():
+    my_date = datetime.now()
+    return my_date
+
+my_date_aqui = my_function()
+print("Data:", my_date_aqui)
+
+
+
